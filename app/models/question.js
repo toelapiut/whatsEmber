cf3577aabd09cd5e235a0d7e99f8ac8bfd982c33
toelapiut <toelapiut7@gmail.com>
2017-09-22 16:36:37 +0300

    added a attribute to store the data

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    author: DS.attr(),
+    asked: DS.attr(),
+    notes: DS.attr(),
+    answers: DS.hasMany('answer', { async: true })
 });
