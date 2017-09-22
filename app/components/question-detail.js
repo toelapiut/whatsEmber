e385d3fbd759b7454c5d36a7b470173e8facef51
toelapiut <toelapiut7@gmail.com>
2017-09-22 18:09:48 +0300

    addedaction up function

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(question) {
+      if (confirm('Please do not delete me!')) {
+        this.sendAction('destroyQuestion', question);
+      }
+    },
+    destroyAnswer(answer) {
+      this.sendAction('destroyAnswer', answer);
+    }
+  }
 });
