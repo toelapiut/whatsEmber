eecae7954d5f8304ad576b4e8bd6cd4ab14a64e2
toelapiut <toelapiut7@gmail.com>
2017-09-22 17:56:30 +0300

    added a comment

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion() {
+      var params = {
+
+        author: this.get('author') ? this.get('author') : "Anonymous",
+        asked: this.get('asked') ? this.get('asked') : "Where's the question",
+        notes: this.get('notes') ? this.get('notes') : "Sorry no notes"
+      };
+
+    // lines with empty parenthesis eg this.set('author', ''); 
+      this.set('author', '');
+      this.set('asked', '');
+      this.set('asked', '');
+      this.set('notes', '');
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    },
+    cancelQuestionForm() {
+      this.set('addNewQuestion', false);
+    }
+  }
 });
