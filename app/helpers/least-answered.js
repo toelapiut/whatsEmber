e276fad429ba0ae0b58bc9348e66186c137fb56a
toelapiut <toelapiut7@gmail.com>
2017-09-29 14:17:52 +0300

    helper added

--- app/helpers/least-answered.js
+++ app/helpers/least-answered.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
-export function leastAnswered(params/*, hash*/) {
-  var question =params[0];
+export function leastAnswered(params /*, hash*/ ) {
+  var question = params[0];
+  if (question.get('answers').get('length') <= 5) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-certificate" data-toggle="tooltip" title="I need more answers"></span>');
+  }
 }
 
 export default Ember.Helper.helper(leastAnswered);
